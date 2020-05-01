003700f645ee394855a3593709c108f201e07233
PranshuKhandal <41776684+PranshuKhandal@users.noreply.github.com>
2020-05-01 18:17:51 +0530

    Update script.ts

--- puzzle15/script.ts
+++ puzzle15/script.ts
@@ -97,16 +97,19 @@ const game = (function(){
 
     const buttonActions = {
         undo: function() {
+            if ( !game.isCapturing ) return;
             if ( !game.states.length ) return;
             writeState( game.states.pop() );
             writeMoves();
         },
         home: function() {
+            if ( !game.isCapturing ) return;
             game.isChecking = false;
             writeState( game.idealState );
             gameStoped();
         },
         shuffle: function() {
+            if ( !game.isCapturing ) return;
             gameStoped();
             writeState( p15.genRandom() );
             game.isCapturing = true;
@@ -238,7 +241,7 @@ Best time: ${ Math.floor( time / 60 ) }m ${ time % 60 }s` );
         if ( game.isChecking && game.state === "123456789ABCDEF0" ) {
             let moves: number = ( + window.localStorage.getItem("p15: moves") ) || 0;
             let time: number = ( + window.localStorage.getItem("p15: time") ) || 0;
-            window.localStorage.setItem("p15: moves", `${ Math.min( moves, game.states.length ) || game.states.length }`);
+            window.localStorage.setItem("p15: moves", `${ Math.min( moves, game.states.length - 1 ) || game.states.length - 1 }`);
             window.localStorage.setItem("p15: time", `${ Math.min( time, game.timer.read() ) || game.timer.read() }`);
             gameStoped();
             delay(500).then( () => alert( "SOLVED!!" ) );
@@ -258,7 +261,7 @@ Best time: ${ Math.floor( time / 60 ) }m ${ time % 60 }s` );
         game.isCapturing = false;
         game.isStarted = false;
         game.states = [];
-        game.timer.stop();
+        game.timer && game.timer.stop();
     }
 
     function writeTime( time: number ): void {
